a7a2fe8daeed21c60fbf2f0b8e4d9a7fcf6ec107
Andrea <andrea.baron32@gmail.com>
2022-07-05 22:09:54 +0000

    notebook

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Template for Machine Learning projects
+# Lineal regression project
+
+In this project a lineal regression model is bulit to predict the insurance prima for an individual based on different factors (age, sex, region, if the person smokes or not, body mass index and the number of children covered by the insurance).
+
--- src/app.py
+++ src/app.py
@@ -1,4 +1,28 @@
-from utils import db_connect
-engine = db_connect()
+### Lineal regression model to predict the insurance prima for an individual
 
-# your code here
+
+## Import libraries
+
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt  
+import seaborn as sns
+import plotly.express as px
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import statsmodels.api as sm
+from sklearn import metrics
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import make_pipeline  
+
+## Read dataset
+url = 'https://raw.githubusercontent.com/4GeeksAcademy/linear-regression-project-tutorial/main/medical_insurance_cost.csv'
+df = pd.read_csv(url)
+
+## Transform the categorical features into dummies
+# If a feature has n categoriess, it will be transformed into n-1 dummies
+df = pd.get_dummies(df, drop_first=True)
+
+
